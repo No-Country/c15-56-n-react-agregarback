0ae08827207e96227d7046625791c2ee9ecae88d
salomon.cjl <contrerassalomon6@gmail.com>
2023-11-30 21:14:00 -0300

    Haciendo el Register

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import NavBar from './layouts/Navbar'
+import Register from './Pages/Register/Register'
 
 function App() {
   return (
     <>
       <NavBar/>
+      <Register/>
     </>
   )
 }
