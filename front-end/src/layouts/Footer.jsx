9b019bd455443a058c2c4e27ae4a381bb1dde580
salomon.cjl <contrerassalomon6@gmail.com>
2023-12-01 15:32:22 -0300

    Home con layouts unidos

---
+++ front-end/src/layouts/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className="relative w-full text-center  bg-sky-800 text-white p-4">
+      <p>© 2022 HereMeNow!. Designed By Wilson Kinyua.</p>
+    </div>
+  );
+}
