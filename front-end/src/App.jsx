b85ce6f36735cb057cc1cc3f297f9c0450bcb77f
Alejo <alejohurtadotesta@gmail.com>
2023-12-04 10:01:33 -0300

    Se crea la pagina employments, se mejoran algunos estilos de la navbar, se agrega el color del texto del home, se instala react-router

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,16 +2,21 @@ import './App.css'
 import Footer from './layouts/Footer'
 import Home from './Pages/Home/Home'
 import NavBar from './layouts/Navbar'
+import Employments from './Pages/Employments/Employments'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 // import Register from './Pages/Register/Register'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar/>
-      <Home/>
-      {/* <Register/> */}
+      <Routes>
+        <Route exact path='/' element={<Home/>} />    
+        {/* <Route exact path='/register' element={<Register/>} /> */}    
+        <Route exact path='/employments' element={<Employments/>} />    
+      </Routes>
       <Footer/>
-    </>
+    </BrowserRouter>
   )
 }
 
