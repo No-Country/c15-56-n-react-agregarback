6ff0dd49e4ec0516a4bc858f7b062d15a8e74c6c
Salomón <115399253+salomoncjl@users.noreply.github.com>
2023-12-08 01:18:59 -0300

    Dev salo (#7)
    * Se instala tailwind y se crea el layout de ña navbar
    
    * Haciendo el Register
    
    * Home con layouts unidos
    
    * Register casi terminado
    
    * employements details complete
    
    * Login y register terminados
    
    * Router y ventana employment implementada
    
    * Employment agregado y boton atras
    
    * Footer nuevo integrado
    
    * css reseteado
    
    * navbar css renovado
    
    * fixed css
    
    ---------
    
    Co-authored-by: Alejo <alejohurtadotesta@gmail.com>
    Co-authored-by: BenZuma LP <benzum566@gmail.com>

--- front-end/src/layouts/Footer.jsx
+++ front-end/src/layouts/Footer.jsx
@@ -1,8 +1,79 @@
+import { NavLink } from "react-router-dom";
+import './footer.css'
 
 export default function Footer() {
   return (
-    <div className="relative w-full text-center  bg-sky-800 text-white p-4">
-      <p>© 2022 HereMeNow!. Designed By Wilson Kinyua.</p>
+    <div className="container-footer">
+      <div className="section1-footer">
+        <h1 className="title-footer">
+          HereMe<span style={{ color: "rgb(74, 255, 255)" }}>Now</span>!
+        </h1>
+        <div className="section1-nav-footer">
+          <NavLink to={"/"} className="item-nav-footer">
+            Home
+          </NavLink>
+          <NavLink to={"/employments"} className="item-nav-footer">
+            Employments
+          </NavLink>
+          <NavLink to={"/devs"} className="item-nav-footer">
+            Devs
+          </NavLink>
+        </div>
+        <div className="links-contact-footer">
+          <a href="#">Contact Us</a>
+          <a href="#">Technical Support</a>
+          <a href="#">Frequently Asked Questions</a>
+          <a href="#">Join Our Team</a>
+        </div>
+        <div>
+          <h2 className="title-nos-footer">No Country c15-56</h2>
+          <ul className="names-footer">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/salomoncjl/"
+                className="name-footer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Contreras, Salomón
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/alejo-hurtado-testa-53b14a216/"
+                className="name-footer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hurtado Testa, Alejo
+              </a>
+            </li>
+            <li>
+              <a 
+                href="https://www.linkedin.com/in/benzum566"
+                className="name-footer"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Zumaran, Bejamin
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/abdidanielescobarm/"
+                className="name-footer"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Escobar, Abdi Daniel
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="section2-footer">
+        <p className="copy-letras">&copy; 2023 HereMeNow! - All rights reserved.</p>
+      </div>
     </div>
   );
 }
